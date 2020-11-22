6b61b2f28b652fdf3de81521babbe27e32b7b33d
muratjumashev <jumasheff@gmail.com>
2020-11-22 22:23:47 +0600

    Add comment about a bug

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -4,4 +4,7 @@ from django.db import models
 
 
 class User(AbstractNamedUser):
+    # dj-rest-auth bug: in its app_settings it imports serializers.py
+    # which triggers line #151 where UserMode.EMAIL_FIELD is used
+    # In our case, EMAIL_FIELD is useless, but we have to make dj-rest-auth happy
     EMAIL_FIELD = 'email'
