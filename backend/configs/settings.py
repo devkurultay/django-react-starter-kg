31602cc1a3ddaa9cca7fd8d32dc0d25d8c7d79a3
ualmaz <ualmaz@gmail.com>
2020-11-27 22:03:20 +0600

    req.txt is updated, RF variables updated

--- backend/configs/settings.py
+++ backend/configs/settings.py
@@ -91,11 +91,13 @@ TEMPLATES = [
     },
 ]
 
+
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 50,
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'dj_rest_auth.jwt_auth.JWTCookieAuthentication',
+    'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema'
     ),
 }
 
@@ -173,4 +175,3 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-REST_FRAMEWORK = {'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema' }
